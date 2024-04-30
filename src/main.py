67800b74e6540c11a6eb5dc41a7c232c862eb659
Sardor Sharipov <sardor2000sharipov@gmail.com>
2024-04-30 23:30:12 +0300

    update required libs

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import internal.repositories.impl
 import internal.controller.impl
 import internal.pub_sub.impl
 import internal.objects
-import internal.view
+import internal.view.main
 
 _logging_choice_to_loglevel = {
     'DEBUG': logging.DEBUG,
@@ -46,24 +46,24 @@ def main():
 
     logging.debug('initializing storage')
     storage = internal.storages.impl.LocalYDocStorage(args['board-path'])
-
     serialized_objects = storage.get_serialized_objects()
     logging.debug('there are %d objects in storage', len(serialized_objects))
     objects = []
     for serialized_obj in serialized_objects.values():
         objects.append(internal.objects.build_from_serialized(serialized_obj, broker))
-
     logging.info('successfully parsed all objects from storage')
 
     logging.debug('initializing repo')
     repo = internal.repositories.impl.Repository(objects, broker)
 
     logging.debug('initializing controller')
-    controller = internal.controller.impl.Controller(repo, storage, broker)  # noqa
+    controller = internal.controller.impl.Controller(repo, storage, broker)
     logging.debug('initializing tkinter')
-    card_controller = internal.controller.impl.CardController(repo, storage, broker)
-    root = internal.view.main_window.TkinterTk(card_controller, repo, broker)
-    root.mainloop()
+    internal.view.main.main(
+        controller=controller,
+        repo=repo,
+        pub_sub=broker
+    )
     logging.info('shutting down...')
     storage.save()
 
