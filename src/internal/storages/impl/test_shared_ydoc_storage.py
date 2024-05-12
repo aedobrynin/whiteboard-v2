2d938c0c808c3e66122b59ead303da673b43ce7a
Liliana Liakh <lilianalyakh@gmail.com>
2024-05-12 17:47:38 +0300

    update test

--- src/internal/storages/impl/test_shared_ydoc_storage.py
+++ src/internal/storages/impl/test_shared_ydoc_storage.py
@@ -8,9 +8,8 @@ from ypy_websocket import WebsocketServer
 from .shared_ydoc_storage import SharedYDocStorage, SERVER_PORT
 
 
-@pytest.mark.asyncio
+@pytest.mark.asincio
 async def test_shared_ydoc_storage_get_updates():
-    logging.info('START')
     board_name = 'test-board'
     updates = {
         'obj_id_1': {'a1': 'b1'},
@@ -31,25 +30,20 @@ async def test_shared_ydoc_storage_get_updates():
         while not websocket_server.started:
             await asyncio.sleep(0)
 
-        logging.debug('try to connect to server=%s...', storage_1.get_uri_connection())
         async with connect(storage_1.get_uri_connection()) as websocket:
-            logging.info('successfully connected to server')
             async with storage_1.get_websocket_provider(websocket):  # type: ignore
                 await asyncio.sleep(1)
         assert not storage_1.is_empty_updates()
-        for k, v in storage_1.get_updates():
-            logging.info('assert v(%s) = update[k](%s)', v, updates[k])
-            assert v == updates[k]
+        for obj in storage_1.get_updates():
+            assert obj['obj_repr'] == updates[obj['obj_id']]
 
         # client 2
         storage_2 = SharedYDocStorage(board_name)
         assert storage_2.is_empty_updates()
         async with connect(storage_2.get_uri_connection()) as websocket:
-            logging.info('successfully connected to server')
             async with storage_2.get_websocket_provider(websocket):  # type: ignore
                 await asyncio.sleep(1)
 
         assert not storage_2.is_empty_updates()
-        for k, v in storage_2.get_updates():
-            logging.info('assert v(%s) = update[k](%s)', v, updates[k])
-            assert v == updates[k]
+        for obj in storage_2.get_updates():
+            assert obj['obj_repr'] == updates[obj['obj_id']]
