dbbbe593d0d15f0242add824da2c12015d90b1ac
Anton <hashlib@yandex.ru>
2024-05-12 15:07:29 +0300

    checkout submenu.py

--- src/internal/view/modules/submenu/submenu.py
+++ src/internal/view/modules/submenu/submenu.py
@@ -21,14 +21,18 @@ class Submenu:
     _property_widgets: List[ttk.Widget]
     _option_menu: Menu
 
-    def __init__(self, obj_id: str, dependencies: internal.view.dependencies.Dependencies):
+    def __init__(
+        self,
+        obj_id: str,
+        dependencies: internal.view.dependencies.Dependencies
+    ):
         self.obj_id = obj_id
         self._property_widgets: List[ttk.Widget] = []
         self._option_menu: Menu = None
         obj: internal.objects.interfaces.IBoardObjectWithPosition = dependencies.repo.get(obj_id)
         if obj.type == internal.objects.types.BoardObjectType.TEXT:
-            obj_canvas: internal.view.modules.text.TextObject = (
-                dependencies.objects_storage.get_by_id(obj_id)
+            obj_canvas: internal.view.modules.text.TextObject = dependencies.objects_storage.get_by_id(
+                obj_id
             )
             self._property_widgets = obj_canvas.widgets(dependencies)
         else:
@@ -36,45 +40,57 @@ class Submenu:
         self._init_option_menu(dependencies)
         self._subscribe_draw_border(dependencies)
 
-    def _subscribe_draw_border(self, dependencies: internal.view.dependencies.Dependencies):
-        # TODO: unsubscribe
+    def _subscribe_draw_border(
+        self, dependencies: internal.view.dependencies.Dependencies
+    ):
         dependencies.pub_sub_broker.subscribe(
             _SUBMENU_PREFIX + self.obj_id,
             self.obj_id,
             internal.objects.events.EVENT_TYPE_OBJECT_CHANGED_SIZE,
-            lambda *_: self._draw_border(dependencies),
+            lambda *_: self._draw_border(dependencies)
         )
 
-    def _draw_border(self, dependencies: internal.view.dependencies.Dependencies):
+    def _draw_border(
+        self, dependencies: internal.view.dependencies.Dependencies
+    ):
         obj = dependencies.objects_storage.get_opt_by_id(self.obj_id)
         if obj and obj.get_focused(dependencies):
             obj.draw_object_border(dependencies)
 
-    def _remove_border(self, dependencies: internal.view.dependencies.Dependencies):
+    def _remove_border(
+        self, dependencies: internal.view.dependencies.Dependencies
+    ):
         obj = dependencies.objects_storage.get_opt_by_id(self.obj_id)
         if obj:
             obj.remove_object_border(dependencies)
             obj.set_focused(dependencies, False)
 
-    def _init_option_menu(self, dependencies: internal.view.dependencies.Dependencies):
+    def _init_option_menu(
+        self, dependencies: internal.view.dependencies.Dependencies
+    ):
         self._option_menu = Menu(None, tearoff=0)
         self._option_menu.add_command(
             label=_BRING_TO_FRONT_DESC, command=lambda: self._bring_to_front(dependencies)
         )
         self._option_menu.add_command(
-            label=_SEND_TO_BACK_DESC, command=lambda: self._send_to_back(dependencies)
-        )
+            label=_SEND_TO_BACK_DESC, command=lambda: self._send_to_back(dependencies))
         self._option_menu.add_command(
             label=_DELETE_DESC, command=lambda: self._delete(dependencies)
         )
 
-    def _bring_to_front(self, dependencies: internal.view.dependencies.Dependencies):
+    def _bring_to_front(
+        self, dependencies: internal.view.dependencies.Dependencies
+    ):
         dependencies.canvas.tag_raise(self.obj_id)
 
-    def _send_to_back(self, dependencies: internal.view.dependencies.Dependencies):
+    def _send_to_back(
+        self, dependencies: internal.view.dependencies.Dependencies
+    ):
         dependencies.canvas.tag_lower(self.obj_id)
 
-    def _delete(self, dependencies: internal.view.dependencies.Dependencies):
+    def _delete(
+        self, dependencies: internal.view.dependencies.Dependencies
+    ):
         self.destroy_menu(dependencies)
         dependencies.controller.delete_object(self.obj_id)
         self.obj_id = None
@@ -87,7 +103,9 @@ class Submenu:
     ):
         self._option_menu.tk_popup(event.x_root, event.y_root, 0)
 
-    def show_menu(self, dependencies: internal.view.dependencies.Dependencies):
+    def show_menu(
+        self, dependencies: internal.view.dependencies.Dependencies
+    ):
         obj = dependencies.objects_storage.get_opt_by_id(self.obj_id)
         if obj:
             obj.set_focused(dependencies, True)
@@ -95,7 +113,9 @@ class Submenu:
         for w in self._property_widgets:
             w.pack(pady=1, fill='both')
 
-    def destroy_menu(self, dependencies: internal.view.dependencies.Dependencies):
+    def destroy_menu(
+        self, dependencies: internal.view.dependencies.Dependencies
+    ):
         obj = dependencies.objects_storage.get_opt_by_id(self.obj_id)
         if obj:
             obj.set_focused(dependencies, False)
@@ -103,4 +123,3 @@ class Submenu:
         self._option_menu.destroy()
         for w in self._property_widgets:
             w.destroy()
-        dependencies.canvas.focus_set()
